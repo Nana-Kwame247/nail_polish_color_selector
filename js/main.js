abf46275d5bc0832b427bf136f02ae038cef9273
Kwame Obeng Yeboah <50833213+Nana-Kwame247@users.noreply.github.com>
2022-04-06 12:32:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+document.getElementById("blue").onclick = nailBlue;
+document.getElementById("indigo").onclick = nailIndigo;
+document.getElementById("purple").onclick = nailPurple;
+document.getElementById("pink").onclick = nailPink;
+document.getElementById("red").onclick = nailRed;
+document.getElementById("orange").onclick = nailOrange;
+document.getElementById("yellow").onclick = nailYellow;
+document.getElementById("green").onclick = nailGreen;
+document.getElementById("teal").onclick = nailTeal;
+document.getElementById("cyan").onclick = nailCyan;
+document.getElementById("gray").onclick = nailGray;
+
+function nailBlue() {
+  document.querySelector(".nail").style.backgroundColor = "#0d6efd";
+}
+function nailIndigo() {
+  document.querySelector(".nail").style.backgroundColor = "#6610f2";
+}
+function nailPurple() {
+  document.querySelector(".nail").style.backgroundColor = "#6f42c1";
+}
+function nailPink() {
+  document.querySelector(".nail").style.backgroundColor = "#d63384";
+}
+
+function nailRed() {
+  document.querySelector(".nail").style.backgroundColor = "#dc3545";
+}
+function nailOrange() {
+  document.querySelector(".nail").style.backgroundColor = "#fd7e14";
+}
+function nailYellow() {
+  document.querySelector(".nail").style.backgroundColor = "#ffc107";
+}
+
+function nailGreen() {
+  document.querySelector(".nail").style.backgroundColor = "#198754";
+}
+
+function nailTeal() {
+  document.querySelector(".nail").style.backgroundColor = "#20c997";
+}
+
+function nailCyan() {
+  document.querySelector(".nail").style.backgroundColor = "#0dcaf0";
+}
+
+function nailGray() {
+  document.querySelector(".nail").style.backgroundColor = "#adb5bd";
+}
